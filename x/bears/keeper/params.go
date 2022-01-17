876b4e8cf8ba6204b93490886bb4141051bb89c3
Roman C <githubroman@yandex.ru>
2022-01-17 15:59:48 +0300

    Add SetNamePrice params

--- x/bears/keeper/params.go
+++ x/bears/keeper/params.go
@@ -7,10 +7,18 @@ import (
 
 // GetParams get all parameters as types.Params
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams()
+	return types.NewParams(
+		k.SetNamePrice(ctx),
+	)
 }
 
 // SetParams set the params
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
+}
+
+// Asdasd returns the Asdasd param
+func (k Keeper) SetNamePrice(ctx sdk.Context) (res sdk.Coin) {
+	k.paramstore.Get(ctx, types.KeySetNamePrice, &res)
+	return
 }
--- x/bears/types/params.go
+++ x/bears/types/params.go
@@ -1,39 +1,71 @@
 package types
 
 import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
 
-// ParamKeyTable the param key table for launch module
-func ParamKeyTable() paramtypes.KeyTable {
-	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
-}
+var (
+	KeySetNamePrice = []byte("SetNamePrice")
+	DefaultSetNamePrice sdk.Coin = sdk.NewCoin("honey", sdk.NewInt(100))
+)
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
-}
-
-// DefaultParams returns a default set of parameters
-func DefaultParams() Params {
-	return NewParams()
+func NewParams(
+	setNamePrice sdk.Coin,
+) Params {
+	return Params{
+		SetNamePrice: setNamePrice,
+	}
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeySetNamePrice, &p.SetNamePrice, validateSetNamePrice),
+	}
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+	if err := validateSetNamePrice(p.SetNamePrice); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+// ParamKeyTable the param key table for launch module
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable(
+		paramtypes.NewParamSetPair(KeySetNamePrice, sdk.Coin{}, validateSetNamePrice),
+	)
+}
+
+// DefaultParams returns a default set of parameters
+func DefaultParams() Params {
+	return NewParams(DefaultSetNamePrice)
 }
 
 // String implements the Stringer interface.
 func (p Params) String() string {
 	out, _ := yaml.Marshal(p)
 	return string(out)
+}
+
+func validateSetNamePrice(i interface{}) error {
+	v, ok := i.(sdk.Coin)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	if !v.IsValid() {
+		return fmt.Errorf("invalid set_name_price: %s", v)
+	}
+
+	return nil
 }
